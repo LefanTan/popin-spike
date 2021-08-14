74a5429b97929c59e0bea4f10d4db5fc355e4180
Lefan <tan6@ualberta.ca>
2021-08-14 01:01:36 -0600

    tailwind custom styles working

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import tw from 'tailwind-react-native-classnames';
+import ctw from './tailwind-custom';
 import {
   Text, View,
 } from 'react-native';
 
 const App = () => {
   return (
-    <View style={tw`bg-blue-100`}>
+    <View style={ctw`bg-dark-green`}>
       <Text style={{fontSize: 20}}>Hey!</Text>
     </View>
   );
