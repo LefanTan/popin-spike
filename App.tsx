7ab6ae0a2231961dcbd01d6669647aac40a97e0e
Lefan <tan6@ualberta.ca>
2021-08-14 19:45:07 -0600

    Testing tw

--- App.tsx
+++ App.tsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import tw from 'tailwind-react-native-classnames';
 import ctw from './tailwind-custom';
 import {
-  Text, Touchable, TouchableOpacity, View,
+  Button,
+  Text, Touchable, TouchableHighlight, TouchableOpacity, View,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
@@ -14,12 +15,14 @@ const App = () => {
     // <NativeBaseProvider>
     <View style={tw`bg-white flex justify-center items-center flex-1`}>
       <View style={tw`flex flex-row items-center`}>
-        <Icon.Button name="facebook" backgroundColor="#96e1ff" onPress={() => setCount(count + 1)}>
-          Facebook!
-        </Icon.Button>
+        <TouchableHighlight onPress={() => setCount(count + 1)} underlayColor="#ff6993" activeOpacity={1} style={tw`bg-red-200 p-1 rounded-md`}>
+          <Icon name="refresh" size={20} style={tw`p-1 text-white`}>
+            <Text style={tw`text-sm ml-10 text-white h-full`}>Increasing count!</Text>
+          </Icon>
+        </TouchableHighlight>
         <TouchableOpacity onPress={() => setCount(0)} style={tw`flex flex-row items-center bg-blue-200 p-1 rounded-md`}>
-          <Icon name="refresh" size={20} style={tw`p-1`}/>
-          <Text style={tw`text-sm`}>Refresh count!</Text>
+          <Icon name="refresh" size={20} style={tw`p-1 text-white`} />
+          <Text style={tw`text-sm text-white`}>Refresh count!</Text>
         </TouchableOpacity>
       </View>
       <Text style={tw`text-4xl`}>Cool beans: {count}</Text>
