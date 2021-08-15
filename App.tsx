f4278cd2136b36f8a911c2554b9701e63b6dce08
Lefan <tan6@ualberta.ca>
2021-08-14 18:39:00 -0600

    update Readme.md

--- App.tsx
+++ App.tsx
@@ -4,12 +4,16 @@ import ctw from './tailwind-custom';
 import {
   Text, View,
 } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
 
 const App = () => {
   return (
-    <View style={ctw`bg-off-white`}>
-      <Text style={{fontSize: 20}}>Hey!</Text>
-    </View>
+      // For adding new dependencies, themes etc
+      <NativeBaseProvider>
+        <View>
+          <Text>TESTING</Text>
+        </View>
+      </NativeBaseProvider>
   );
 };
 
