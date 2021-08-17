92f45b67ef1ed0f923e0efb0caf21e26e40ead55
Lefan <tan6@ualberta.ca>
2021-08-17 00:23:24 -0600

    basic map works for android

--- src/screens/DiscoverScreen.tsx
+++ src/screens/DiscoverScreen.tsx
@@ -1,12 +1,20 @@
-import { Center, Text } from 'native-base';
+import { Center, Text } from 'native-base'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
 import React from 'react'
 
-interface DiscoverScreenProps {}
+interface DiscoverScreenProps { }
 
-export const DiscoverScreen: React.FC<DiscoverScreenProps> = ({}) => {
-        return (
-            <Center flex={1}>
-                <Text>Discover</Text>
-            </Center>
-        );
+export const DiscoverScreen: React.FC<DiscoverScreenProps> = ({ }) => {
+    return (
+        <MapView
+            provider={PROVIDER_GOOGLE}
+            style={{width: '100%', height: '100%'}}
+            initialRegion={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            }}
+        />
+    );
 }
