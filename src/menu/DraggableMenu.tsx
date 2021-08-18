2d71f67c75cf4257d87ff74987dbb1b74a4ade03
Lefan <tan6@ualberta.ca>
2021-08-17 22:36:59 -0600

    working

--- src/menu/DraggableMenu.tsx
+++ src/menu/DraggableMenu.tsx
@@ -1,27 +1,25 @@
-import { Box, Button, Image, useTheme } from 'native-base';
-import React, { useState } from 'react'
-import { PanGestureHandler } from 'react-native-gesture-handler';
+import { Box, useTheme } from 'native-base';
+import React from 'react'
+import { PanGestureHandler} from 'react-native-gesture-handler';
 import Animated, { useSharedValue, useAnimatedStyle, useAnimatedGestureHandler, withSpring } from 'react-native-reanimated';
 import { clamp } from 'react-native-redash';
 import { useWindowDimensions } from 'react-native'
 
 interface DraggableMenuProps {
-    minHeight: number
-    maxHeight: number
+    minHeightOffset: number
+    maxHeightOffset: number
     snapPositionsInPercentage: number[]
 }
 
 export const DraggableMenu: React.FC<DraggableMenuProps> = (props) => {
-    // True when the drag bar is being touched
-    const [startDrag, setStartDrag] = useState(false)
     const { height } = useWindowDimensions()
     // useTheme retrieves the theme object from NativeBase
     const { colors } = useTheme()
 
-    const maxHeight = height - props.maxHeight
-    const minHeight = props.minHeight
-    const yMenu = useSharedValue(maxHeight)
-    const yMenuSnapPositions = props.snapPositionsInPercentage.map((percent, index) => index === 0 ? minHeight : percent * maxHeight)
+    const maxHeightOffset = props.maxHeightOffset
+    const minHeightOffset = props.minHeightOffset
+    const yMenu = useSharedValue(maxHeightOffset)
+    const yMenuSnapPositions = props.snapPositionsInPercentage.map((percent, index) => index === 0 ? minHeightOffset : percent * maxHeightOffset)
 
     // Animated style for the View
     const draggableMenuStyle = useAnimatedStyle(() => {
@@ -37,47 +35,38 @@ export const DraggableMenu: React.FC<DraggableMenuProps> = (props) => {
             ctx.startY = yMenu.value
         },
         onActive: (evt, ctx) => {
-            // true if the drag bar is being touch
-            if (startDrag) {
-                // clamp the value so it doesn't go below or over the limit
-                let draggedVal = clamp(ctx.startY + evt.translationY, minHeight, maxHeight)
-                // update the limit
-                yMenu.value = draggedVal
-            }
+            // clamp the value so it doesn't go below or over the limit
+            let draggedVal = clamp(ctx.startY + evt.translationY, minHeightOffset, maxHeightOffset)
+            // update the limit
+            yMenu.value = draggedVal
         },
         onEnd: () => {
-            if (startDrag) {
-                // On release, snap the View to the closest snap position
-                let closestVal = yMenuSnapPositions.reduce((a, b) => {
-                    return Math.abs(a - yMenu.value) > Math.abs(b - yMenu.value) ? b : a
-                })
-                // WithSpring() adds a 'spring' effect to the drag animation 
-                yMenu.value = withSpring(closestVal, { stiffness: 200, damping: 20 })
-            }
+            // On release, snap the View to the closest snap position
+            let closestVal = yMenuSnapPositions.reduce((a, b) => {
+                return Math.abs(a - yMenu.value) > Math.abs(b - yMenu.value) ? b : a
+            })
+            // WithSpring() adds a 'spring' effect to the drag animation 
+            yMenu.value = withSpring(closestVal, { stiffness: 200, damping: 20 })
         }
     })
 
-    // Events for press in and out of the drag bar
-    const onBarTouchStart = () => setStartDrag(true)
-    const onBarTouchEnd = () => setStartDrag(false)
-    console.log(startDrag)
     return (
         // Make the Animated.View draggble
-        <PanGestureHandler onGestureEvent={menuGestureHandler} >
-            <Animated.View
-                style={[draggableMenuStyle, {
-                    backgroundColor: colors['primary']['400'], height: height,
-                    borderTopLeftRadius: 20, borderTopRightRadius: 20,
-                    position: 'absolute', left: 0, right: 0
-                }]}
-            >
-                {/* Drag Bar */}
-                <Button android_disableSound={true} _pressed={{ bg: 'transparent' }}
-                    height='3%' display="flex" bg="transparent" padding={0}>
+        <Animated.View
+            style={[draggableMenuStyle, {
+                backgroundColor: colors['primary']['400'], height: height,
+                borderTopLeftRadius: 20, borderTopRightRadius: 20,
+                position: 'absolute', left: 0, right: 0
+            }]}
+        >
+            <PanGestureHandler onGestureEvent={menuGestureHandler} >
+                <Animated.View
+                    display='flex' alignItems="center" justifyContent="center"
+                    height='3%' bg="transparent" padding={0}>
                     <Box height={1} width={20} rounded={20} bg="white" />
-                </Button>
-                {props.children}
-            </Animated.View>
-        </PanGestureHandler>
+                </Animated.View>
+            </PanGestureHandler>
+            {props.children}
+        </Animated.View>
     );
 }
