781f435375b1651349ef0e3eb33edaa45a02aed1
Lefan <tan6@ualberta.ca>
2021-08-26 22:40:56 -0600

    back button readjusted

--- src/screens/EventScreen.tsx
+++ src/screens/EventScreen.tsx
@@ -25,29 +25,34 @@ export const EventScreen = ({ navigation, route }: DiscoverStackNavProps<"Event"
     const sameDay = startDate.format('DD MMM') === endDate.format('DD MMM')
 
     const { colors } = useTheme()
-    const headerOpacityValue = useSharedValue(0)
+    const headerOpacityValue = useSharedValue(1)
     const carouselRef = useRef(null)
     var prevScrolled = 0
 
     const headerStyle = useAnimatedStyle(() => {
         return {
-            opacity: headerOpacityValue.value
+            opacity: headerOpacityValue.value,
+            transform: [{ translateX: -100 + headerOpacityValue.value * 100 }]
         }
     })
+
+    const detailIconStyle = ctw.style(`text-secondary-400 -ml-1`, { minWidth: '10%', textAlign: 'center' })
 
     return (
         <Flex height="100%">
             <ScrollView bg="primary.100" onScroll={(event) => {
                 headerOpacityValue.value = withTiming(interpolate(event.nativeEvent.contentOffset.y - prevScrolled, [0, 1], [1, 0], Extrapolate.CLAMP), { duration: 150 })
-                prevScrolled = event.nativeEvent.contentOffset.y
-            }}>
+            }}
+                onScrollEndDrag={(event) => prevScrolled = event.nativeEvent.contentOffset.y}
+            >
                 <VStack>
                     <Center
-                        borderRadius={15} marginTop={16}
+                        borderRadius={15} marginTop={16} width="100%"
                     >
                         <Carousel
                             ref={carouselRef}
                             data={mockPhotos}
+                            vertical={false}
                             renderItem={({ item }) =>
                                 <Image
                                     alt='eventpic' marginRight={2} width={hp(45)} height={hp(27.5)}
@@ -59,14 +64,14 @@ export const EventScreen = ({ navigation, route }: DiscoverStackNavProps<"Event"
                     </Center>
                     <VStack paddingX={5} paddingY={5}>
                         <Heading
-                            fontSize={35} fontFamily="heading" fontWeight={600}
+                            fontSize={hp(5)} fontFamily="heading" fontWeight={600}
                             numberOfLines={4} color="primary.700"
                         >
                             {/* Maximum 70 characters */}
                             {route.params.event.eventName}
                         </Heading>
                         <Heading width="90%"
-                            fontSize={20} fontWeight={400} paddingY={3}
+                            fontSize={hp(3)} fontWeight={400} paddingY={3}
                             numberOfLines={2} color="primary.700"
                         >
                             {route.params.event.hostName}
@@ -89,26 +94,26 @@ export const EventScreen = ({ navigation, route }: DiscoverStackNavProps<"Event"
                         <VStack bg="primary.200" borderRadius={20} marginTop={3} paddingX={5} paddingY={4}>
                             <Heading fontWeight={400} fontSize={30}>Details</Heading>
                             <HStack marginTop={3} alignItems='center'>
-                                <Ionicon name="location-sharp" size={hp(4)} style={ctw`text-secondary-400 absolute -ml-1 mr-1`} />
-                                <Text underline width="90%" numberOfLines={2} color="primary.700" marginLeft={9}>{route.params.event.address}</Text>
+                                <Ionicon name="location-sharp" size={hp(4)} style={detailIconStyle} />
+                                <Text underline width="95%" numberOfLines={2} color="primary.700" marginLeft={5}>{route.params.event.address}</Text>
                             </HStack>
                             <HStack marginTop={1} alignItems='center'>
-                                <Ionicon name="people-circle-sharp" size={hp(4)} style={ctw`text-secondary-400 absolute -ml-1 mr-1`} />
-                                <Text numberOfLines={2} color="primary.700" marginLeft={9}>{route.params.event.poppedInAmount} people popped in</Text>
+                                <Ionicon name="people-circle-sharp" size={hp(4)} style={detailIconStyle} />
+                                <Text numberOfLines={2} color="primary.700" marginLeft={5}>{route.params.event.poppedInAmount} people popped in</Text>
                             </HStack>
                             <HStack marginTop={1} alignItems='center'>
-                                <Ionicon name="calendar" size={hp(3)} style={ctw`text-secondary-400 absolute mr-3`} />
-                                <Text flex={2} numberOfLines={2} color="primary.700" marginLeft={9}>
+                                <Ionicon name="calendar" size={hp(3)} style={detailIconStyle} />
+                                <Text flex={2} numberOfLines={2} color="primary.700" marginLeft={5}>
                                     {`${startDate.format('dddd MMM DD : h:MMa')} - ${sameDay ? '' : endDate.format(`dddd MMM DD :`)} ${endDate.format(`h:MMa`)}`}
                                 </Text>
                             </HStack>
                             {route.params.event.price && <HStack marginTop={1} alignItems='center'>
-                                <FoundationIcon name="dollar" size={hp(5)} style={ctw`text-secondary-400 absolute ml-1 mr-4`} />
-                                <Text numberOfLines={2} color="primary.700" marginLeft={9}>CAD {route.params.event.price}</Text>
+                                <FoundationIcon name="dollar" size={hp(5)} style={detailIconStyle} />
+                                <Text numberOfLines={2} color="primary.700" marginLeft={5}>CAD {route.params.event.price}</Text>
                             </HStack>}
                             {route.params.event.website && <HStack marginTop={1} alignItems='center'>
-                                <MaterialCommunityIcon name="web" size={hp(3.5)} style={ctw`text-secondary-400 absolute mr-4`} />
-                                <Text underline numberOfLines={1} color="primary.700" marginLeft={9}>{route.params.event.website}</Text>
+                                <MaterialCommunityIcon name="web" size={hp(3.5)} style={detailIconStyle} />
+                                <Text underline numberOfLines={1} color="primary.700" marginLeft={5}>{route.params.event.website}</Text>
                             </HStack>}
                             <Text color="primary.700" marginTop={2}>{route.params.event.description}</Text>
                         </VStack>
@@ -117,10 +122,10 @@ export const EventScreen = ({ navigation, route }: DiscoverStackNavProps<"Event"
             </ScrollView>
 
             <Animated.View
-                style={[headerStyle, ctw`bg-transparent flex flex-row items-center absolute w-full h-16`]}
+                style={[headerStyle, ctw`bg-transparent flex flex-row items-center absolute w-full`]}
             >
                 <Pressable
-                    style={ctw.style(`ml-2 bg-secondary-400 flex justify-center items-center`, { width: hp(6), height: hp(6), borderRadius: 50 })}
+                    style={ctw.style(`ml-2 mt-2 bg-secondary-400 flex justify-center items-center`, { width: hp(6), height: hp(6), borderRadius: 50 })}
                     onPress={() => navigation.goBack()} _pressed={{ bg: colors['secondary']['500'] }}
                 >
                     {({ isPressed }) =>
