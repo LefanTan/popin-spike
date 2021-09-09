f2159cb15ccea1f62de1300ff11c9e21041f0ba4
Lefan <tan6@ualberta.ca>
2021-09-09 00:09:49 -0600

    details page added except for photos

--- src/components/CreateEventInput.tsx
+++ src/components/CreateEventInput.tsx
@@ -1,6 +1,6 @@
-import {Heading, HStack, Input, Pressable, Text, useTheme, View, VStack} from "native-base";
+import {Heading, HStack, Input, Text, View, VStack} from "native-base";
 import React, {useState} from "react";
-import {StyleProp, ViewStyle} from "react-native";
+import {KeyboardTypeOptions, StyleProp, ViewStyle} from "react-native";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
@@ -9,37 +9,67 @@ import {
 interface CreateEventInputProps {
   content: string;
   title: string;
-  optional?: boolean;
   onChangeText: (text: string) => void;
+
   viewStyle?: StyleProp<ViewStyle>;
+  inputStyle?: StyleProp<ViewStyle>;
+
+  /* TextInput props */
+  numberOfLines?: number;
+  multiline?: boolean;
+  prefix?: string;
+  maxLength?: number;
+  optional?: boolean;
+  keyboardType?: KeyboardTypeOptions;
 }
 
 export const CreateEventInput: React.FC<CreateEventInputProps> = props => {
   return (
     <View width="100%" style={props.viewStyle}>
-      {props.optional && <Text marginBottom={1.5}>Optional</Text>}
       <HStack bg="primary.200" borderRadius={10} paddingY={2} paddingX={3} alignItems="center">
         <VStack marginRight="auto" width="100%">
-          <Heading fontSize={hp(2)} color="primary.500" fontWeight={600}>
-            {props.title}
-          </Heading>
           <HStack alignItems="center">
-            <Text
-              marginTop={-1}
-              marginRight={1}
-              fontWeight={500}
-              fontSize={hp(2.5)}
-              color="primary.700">
-              $
-            </Text>
+            <Heading fontSize={hp(2)} color="primary.500" fontWeight={600}>
+              {props.title}
+            </Heading>
+            {props.optional && (
+              <Text
+                fontWeight={400}
+                color="secondary.400"
+                marginLeft={2}
+                marginBottom={0.5}
+                fontSize={hp(1.75)}>
+                Optional
+              </Text>
+            )}
+          </HStack>
+
+          <HStack alignItems="center">
+            {props.prefix && (
+              <Text
+                marginTop={-1}
+                marginRight={1}
+                fontWeight={500}
+                fontSize={hp(2.5)}
+                color="primary.700">
+                {props.prefix}
+              </Text>
+            )}
             <Input
               fontSize={hp(2.5)}
+              fontWeight={400}
               color="primary.700"
               width="100%"
               variant="unstyled"
               padding={0}
-              marginTop={-1}
-              keyboardType="number-pad"
+              marginTop={0}
+              textAlignVertical="top"
+              multiline={props.multiline}
+              numberOfLines={props.numberOfLines}
+              maxLength={props.maxLength}
+              keyboardType={props.keyboardType}
+              returnKeyType="done"
+              blurOnSubmit={true}
               value={props.content}
               onChangeText={props.onChangeText}
             />
--- src/screens/createEventPages/DetailsPage.tsx
+++ src/screens/createEventPages/DetailsPage.tsx
@@ -1,4 +1,4 @@
-import {Heading, VStack} from "native-base";
+import {Heading, ScrollView, Text, VStack} from "native-base";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
@@ -9,19 +9,46 @@ import {CreateEventInput} from "../../components/CreateEventInput";
 interface DetailsPageProps {}
 
 export const DetailsPage: React.FC<DetailsPageProps> = () => {
+  const maxDiscLength = 400;
   const [price, setPrice] = useState<number | undefined>();
+  const [website, setWebsite] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
 
   return (
     <VStack paddingY={3} paddingX={4} flex={1}>
-      <Heading fontSize={hp(4.5)} fontWeight={600} marginBottom={3}>
-        Details
-      </Heading>
-      <CreateEventInput
-        title="Price"
-        onChangeText={text => setPrice(text === "" ? undefined : parseInt(text))}
-        content={price !== undefined ? price?.toString() : ""}
-        optional
-      />
+      <ScrollView>
+        <Heading fontSize={hp(4.5)} fontWeight={600} marginBottom={3}>
+          Details
+        </Heading>
+        <CreateEventInput
+          title="Event Description"
+          onChangeText={setDescription}
+          content={description}
+          viewStyle={{marginTop: 0}}
+          numberOfLines={5}
+          maxLength={maxDiscLength}
+          multiline
+        />
+        <Text fontSize={hp(2.5)} fontWeight={500} width="100%" textAlign="right">
+          characters left: {maxDiscLength - description.length}
+        </Text>
+        <CreateEventInput
+          title="Price"
+          onChangeText={text => setPrice(text === "" ? undefined : parseInt(text, 10))}
+          content={price !== undefined ? price?.toString() : ""}
+          prefix="$"
+          keyboardType="number-pad"
+          viewStyle={{marginTop: 10}}
+          optional
+        />
+        <CreateEventInput
+          title="Website"
+          onChangeText={setWebsite}
+          content={website}
+          optional
+          viewStyle={{marginTop: 10}}
+        />
+      </ScrollView>
     </VStack>
   );
 };
